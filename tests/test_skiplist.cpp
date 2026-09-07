3baaf4892ecda0901d47afd9c0f1849b7716fcd7
Hamsavardhan <hamsavardhanom@gmail.com>
2026-09-07 14:53:49 +0000

    Edit the test_skiplist to match the pattern of other test files

--- tests/test_skiplist.cpp
+++ tests/test_skiplist.cpp
@@ -1,47 +1,65 @@
+#include <gtest/gtest.h>
 #include "engine/region_allocator.hpp"
 #include "engine/skiplist.hpp"
 
-#include <iostream>
-#include <cassert>
 #include <string>
 
-int main()
+namespace
+{
+
+TEST(SkipListTest, HandlesPointInsertsAndQueries)
 {
     engine::RegionAllocator allocator;
     engine::SkipList list(allocator);
 
-    // 1. Point inserts
     list.Put("Atta_5kg", "120");
     list.Put("Basmati_Rice", "250");
     list.Put("Tata_Salt", "30");
 
-    // 2. Fetch point records
     auto val1 = list.Get("Atta_5kg");
     auto val2 = list.Get("Basmati_Rice");
     auto val3 = list.Get("Tata_Salt");
     auto missing = list.Get("NonExistent_Item");
 
-    // Assert makes sure that error is thrown if the conditon is invalidated. If we get a error free console msg, build succeeded
-    assert(val1.has_value() && *val1 == "120");
-    assert(val2.has_value() && *val2 == "250");
-    assert(val3.has_value() && *val3 == "30");
-    assert(!missing.has_value());
+    ASSERT_TRUE(val1.has_value());
+    EXPECT_EQ(*val1, "120");
 
-    // 3. In-place update test
+    ASSERT_TRUE(val2.has_value());
+    EXPECT_EQ(*val2, "250");
+
+    ASSERT_TRUE(val3.has_value());
+    EXPECT_EQ(*val3, "30");
+
+    EXPECT_FALSE(missing.has_value());
+}
+
+TEST(SkipListTest, HandlesInPlaceUpdates)
+{
+    engine::RegionAllocator allocator;
+    engine::SkipList list(allocator);
+
+    list.Put("Atta_5kg", "120");
     list.Put("Atta_5kg", "119");
-    auto updated_val = list.Get("Atta_5kg");
-    assert(updated_val.has_value() && *updated_val == "119");
 
-    // 4. Memory ownership test: pass temporary out-of-scope strings
+    auto updated_val = list.Get("Atta_5kg");
+    ASSERT_TRUE(updated_val.has_value());
+    EXPECT_EQ(*updated_val, "119");
+}
+
+TEST(SkipListTest, PersistsAcrossScopeDestruction)
+{
+    engine::RegionAllocator allocator;
+    engine::SkipList list(allocator);
+
     {
         std::string temp_key = "Transient_Milk";
         std::string temp_val = "60";
         list.Put(temp_key, temp_val);
-    } // temp_key and temp_val fall out of scope and are destroyed here
+    }
 
     auto persistent_val = list.Get("Transient_Milk");
-    assert(persistent_val.has_value() && *persistent_val == "60");
-
-    std::cout << "\n>>> ALL SKIPLIST IN-MEMORY TESTS PASSED! <<<\n\n";
-    return 0;
+    ASSERT_TRUE(persistent_val.has_value());
+    EXPECT_EQ(*persistent_val, "60");
 }
+
+} // namespace
